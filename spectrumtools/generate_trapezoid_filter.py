042cceca505303f30cec87c8d693504d2d64da12
iankolaja <iankolaja@gmail.com>
2022-10-07 15:16:48 -0700

    Simplified filter

--- spectrumtools/generate_trapezoid_filter.py
+++ spectrumtools/generate_trapezoid_filter.py
@@ -1,23 +1,10 @@
 import numpy as np
 
 
-def _truncated_ramp(t, k):
-    end_index = int(np.where(t == k)[0][0])+1
-    rise = t[0:end_index]
-    fall = np.zeros(len(t)-np.argmin(np.abs(t-k))-1)
-    return np.concatenate((rise,fall))
-
-
-def _moving_average(t, l):
-    return np.concatenate((np.ones(np.argmin(np.abs(t-l))+1),
-            np.zeros(len(t)-np.argmin(np.abs(t-l))-1)))
-
-
 def generate_trapezoid_filter(tau, k, l):
-    t = np.arange(0, k+l)
-    rising_ramp = _truncated_ramp(t,k)
-    first_average = _moving_average(t,l)
-    second_average = np.roll(_truncated_ramp(t, k), l)
-    descending_ramp = np.roll(_moving_average(t, l), k)
-    filter_shape = rising_ramp + tau*first_average + (k-tau)*descending_ramp - second_average
+    rising_ramp = np.ones(k)*np.exp(np.arange(k)/tau)
+    first_average = np.zeros(l)
+    descending_ramp = -np.ones(k)*np.exp(np.flip(np.arange(k))/tau)
+    filter_shape = np.hstack((rising_ramp,first_average,descending_ramp))
     return filter_shape
+
