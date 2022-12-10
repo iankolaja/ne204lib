d8a43c4acad35295935c2a3bf85d0cee98382a6a
iankolaja <iankolaja@gmail.com>
2022-12-10 14:51:01 -0800

    Return amplitude values

--- kit204lib/make_histogram.py
+++ kit204lib/make_histogram.py
@@ -2,18 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def make_histogram(trapezoid_heights, num_bins, upper_noise_cutoff=0.995, do_plot=False):
+def make_histogram(trapezoid_heights, num_bins, upper_noise_cutoff=0.995, do_plot=False,
+                   log_plot = True):
     spectrum_start = np.min(trapezoid_heights)
     spectrum_end = np.quantile(trapezoid_heights, upper_noise_cutoff)
 
 
-    counts, bins = np.histogram(trapezoid_heights,
+    counts, amplitude_bins = np.histogram(trapezoid_heights,
                                 bins=num_bins,
                                 range=[spectrum_start, spectrum_end])
     channels = np.arange(1, 1+len(counts))
+    amplitude_bins = (amplitude_bins[1:] + amplitude_bins[:-1]) / 2
     if do_plot:
         plt.plot(channels, counts)
         plt.xlabel("Channel")
         plt.ylabel("Counts")
+        if log_plot:
+            plt.yscale("log")
         plt.show()
-    return counts
+    return counts, amplitude_bins
